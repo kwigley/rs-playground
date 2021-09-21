ee53ba751d8e76b0ec71522c788242c63c6ba3f6
Kyle Wigley <kwigley44@gmail.com>
2021-09-21 00:03:53 -0400

    add test for draw game state

--- ttt/tests/test.rs
+++ ttt/tests/test.rs
@@ -21,3 +21,22 @@ fn test_winning_state() {
     let result = game.play();
     assert_eq!(result, GameResult::Win(Player::Ex))
 }
+
+#[test]
+fn test_draw_state() {
+    let mut board = HashMap::new();
+    board.insert((X, X), Player::Ex);
+    board.insert((X, Y), Player::Oh);
+    board.insert((Y, X), Player::Ex);
+    board.insert((Y, Y), Player::Oh);
+    board.insert((Y, Z), Player::Ex);
+    board.insert((Z, X), Player::Oh);
+    board.insert((Z, Y), Player::Ex);
+    board.insert((Z, Z), Player::Oh);
+    let game = Game {
+        board,
+        current_player: Player::Oh,
+    };
+    let result = game.play();
+    assert_eq!(result, GameResult::Draw)
+}
